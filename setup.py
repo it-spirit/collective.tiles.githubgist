5b860de26e5345fda87c4e7e62e462c385313bcf
Thomas Massmann <thomas.massmann@it-spir.it>
2017-06-04 13:28:23 -0400

    Mosaic required Plone > 5. Remove 4.x compatibility.

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone",
-        "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
